86b32dda0924442dee3dab5acc687172c7aa8bc4
Paul <lamkashingpaul@gmail.com>
2020-03-17 09:28:40 +0800

    Update Required Package List

--- deceptive_train.py
+++ deceptive_train.py
@@ -13,6 +13,7 @@ def main():
     import pickle
 
     import nltk
+    nltk.download('wordnet')
     nltk.download('stopwords')
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
